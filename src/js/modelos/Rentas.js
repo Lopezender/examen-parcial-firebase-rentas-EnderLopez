87655f5120975114f62fa67e62ce953e58fe08ee
Ender Lopez <est.ealemanl0201@uml.edu.ni>
2023-05-20 18:42:10 -0600

    N

--- src/js/modelos/Rentas.js
+++ src/js/modelos/Rentas.js
@@ -15,12 +15,12 @@ let Rentas_contenedor = document.getElementById('Rentas');
 querySnapshot.forEach((doc) => { 
   let Rentas = doc.data() 
     Rentas_contenedor.innerHTML += `<li> 
-    <div class="w-80 h-full mx-auto bg-white  mb-10 pb-2 rounded-md inline-flex mt-8 ml-10"> 
+    <div class="w-80 h-full mx-auto bg-white  mb-10 pb-8 rounded-md inline-flex mt-8 ml-10"> 
     <div class=""> 
         <img src="${Rentas.foto}" alt="${Rentas.nombre}" class="rounded-xl mx-auto pl-4 pr-4 mt-8"> 
         </div> 
         </div> 
-        <div class="mt-4 text-black font-serif inline-block ml-20"> 
+        <div class="h-full mx-auto bg-white pb-2 mt-4 rounded-2xl text-black font-serif inline-block ml-20"> 
         
         <div class="text-center">${Rentas.precio} </div>  
         <div class="text-block">${Rentas.baños} baños 
--- src/js/paginas/barra.js
+++ src/js/paginas/barra.js
@@ -4,7 +4,7 @@ barra.innerHTML = `<div
 class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-2 left-1/2 dark:bg-gray-700 dark:border-gray-600">
 <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
   
-  <a data-tooltip-target="tooltip-home" href= "index.html"
+  <a data-tooltip-target="tooltip-home" 
     class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
     <svg class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
       <path
@@ -20,7 +20,7 @@ class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-g
     <div class="tooltip-arrow" data-popper-arrow></div>
   </div>
 
-  <a data-tooltip-target="tooltip-eventos" href= "detalles.html"
+  <a data-tooltip-target="tooltip-eventos"
     class="inline-flex flex-col items-center justify-center px-5 text-blue-600 hover:bg-gray-50 dark:hover:bg-gray-800 group">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -38,8 +38,8 @@ class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-g
   </div>
   <div class="flex items-center justify-center">
   
-    <a data-tooltip-target="tooltip-new" href="Tickets.html"
-      class="inline-flex items-center justify-center w-10 h-10 font-medium rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+    <a data-tooltip-target="tooltip-new"
+      class="inline-flex items-center justify-center w-10 h-10 font-medium rounded-full hover:bg-green-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
 </svg>
@@ -52,7 +52,7 @@ class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-g
     Comprar ticket
     <div class="tooltip-arrow" data-popper-arrow></div>
   </div>
-  <a data-tooltip-target="tooltip-calendario" href="timeline.html"
+  <a data-tooltip-target="tooltip-calendario"
     class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
@@ -66,7 +66,7 @@ class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-g
     Calendario
     <div class="tooltip-arrow" data-popper-arrow></div>
   </div>
-  <a data-tooltip-target="tooltip-perfil" href="perfil.html"
+  <a data-tooltip-target="tooltip-perfil"
     class="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
